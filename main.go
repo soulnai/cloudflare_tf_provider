b3b7c0bd273685a523ab17e130c1057b9064a9c6
ovasyliev <reg@soulnai@gmail.com>
2025-11-29 23:59:38 +0100

    feat: structure refactoring

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"log"
 
+	"terraform-provider-cloudflare-tunnel/internal/provider"
+
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
 func main() {
-	err := providerserver.Serve(context.Background(), New("1.0.0"), providerserver.ServeOpts{
+	err := providerserver.Serve(context.Background(), provider.New("1.0.0"), providerserver.ServeOpts{
 		Address: "registry.terraform.io/cloudflare/cloudflare-tunnel",
 	})
 
